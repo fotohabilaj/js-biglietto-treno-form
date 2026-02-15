26f5115aac6911ec870275fdc35ce326bbeeeca6
fotohabilaj <fotohabilaj@gmail.com>
2026-02-15 16:11:11 +0100

    add select

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 console.log('hello');
 
-const ageEl = document.getElementById('age');
+const nameEl = document.getElementById('name');
 const distanceEl = document.getElementById('distance');
-const butEl = document.getElementById('button')
-const formEl = document.querySelector('form')
+const butEl = document.getElementById('button');
+const discountEl = document.getElementById('discount');
+const formEl = document.querySelector('form');
 
 formEl.addEventListener('submit', function(e){
     e.preventDefault();
-    let age= ageEl.value;
-    let distanceTot= distanceEl.value;
-    console.log(age,distanceTot);
+    let discount = Number(discountEl.value);
+    let distanceTot = distanceEl.value;
+    let name = nameEl.value;
+    console.log(discount,distanceTot,name);
 })
 
