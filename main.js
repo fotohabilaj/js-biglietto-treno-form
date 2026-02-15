e0e67693af57c039614f2234cd3628a5800023f8
fotohabilaj <fotohabilaj@gmail.com>
2026-02-15 15:35:45 +0100

    milestone 1 complete

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('hello');
+
+const ageEl = document.getElementById('age');
+const distanceEl = document.getElementById('distance');
+const butEl = document.getElementById('button')
+
+butEl.addEventListener('click', function(){
+    let age= ageEl.value;
+    let distanceTot= distanceEl.value;
+    console.log(age,distanceTot);
+})
+
