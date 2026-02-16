7b9c128f4ad6a23f7d5174f8cf318715e9c05cc8
fotohabilaj <fotohabilaj@gmail.com>
2026-02-16 15:30:27 +0100

    milestone 2 complete

--- main.js
+++ main.js
@@ -2,15 +2,31 @@ console.log('hello');
 
 const nameEl = document.getElementById('name');
 const distanceEl = document.getElementById('distance');
-const butEl = document.getElementById('button');
 const discountEl = document.getElementById('discount');
 const formEl = document.querySelector('form');
 
 formEl.addEventListener('submit', function(e){
     e.preventDefault();
-    let discount = Number(discountEl.value);
-    let distanceTot = distanceEl.value;
-    let name = nameEl.value;
-    console.log(discount,distanceTot,name);
-})
+    const discount = Number(discountEl.value);
+    const distanceTot = distanceEl.value;
+    const name = nameEl.value;
+    const ticketPrice = (distanceTot*0.21*discount).toFixed(2)
+    let ticketType;
+    if(discount==0.8){ticketType=('Biglietto under 18')}
+    else if (discount==1){ticketType=('Biglietto adulti')}
+    else{ticketType=('Biglietto over 65')}
+    console.log(discount,distanceTot,name,ticketPrice);
+    
+    const personEl = document.getElementById('person');
+    const ticketEl = document.getElementById('ticket');
+    const carrEl = document.getElementById('carr');
+    const cpcodeEl = document.getElementById('cpcode');
+    const priceEl = document.getElementById('price');
+   
 
+    personEl.innerHTML = name;
+    ticketEl.innerHTML = ticketType;
+    carrEl.innerHTML = Math.floor(Math.random() * 10);
+    cpcodeEl.innerHTML = Math.floor(Math.random() * 100000);
+    priceEl.innerHTML = ticketPrice;
+})
